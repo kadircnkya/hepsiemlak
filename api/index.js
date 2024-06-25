17fc67f800643628bbc6ab271a7c2f10ea4f573f
kadircnkya <a.kadir2056@gmail.com>
2024-06-25 23:52:48 +0300

    fourth commit

--- api/index.js
+++ api/index.js
@@ -18,18 +18,12 @@ mongoose
     console.log(err);
 });
 
-
+const __dirname = path.resolve();
 
 const app=express()
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors(
-    {
-        origin :["https://deploy-mern-1whq.vercel.app"],
-        methods:["POST","GET"],
-        credentials :true
-    }
-));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.listen(3000,()=>{
@@ -42,6 +36,14 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
 
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
+
+  
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
     const message=err.message || 'Internal Server Error';
