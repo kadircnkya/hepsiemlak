7a454d422338715125017e099325adc28e5537bf
kadircnkya <a.kadir2056@gmail.com>
2024-06-27 23:36:16 +0300

    git test

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -26,8 +26,6 @@ export const signin = async (req, res, next) => {
    console.log(req.body);
 
 
-   
-
    try {
       const validUser = await User.findOne({ email });
       if (!validUser) return next(errorHandler(404, "User not found !"));
